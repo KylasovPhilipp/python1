ea7364f83f4fd9cb5a3566c20b6f6e5f1f4beb59
KylasovPhilipp <Kyulasov@yandex.ru>
2020-02-28 11:29:04 +0500

    задания к 1 уроку

---
+++ lesson1.py
@@ -0,0 +1,33 @@
+         task1
+
+
+name = input('What is your name: ')
+age = 16
+city = Moscow
+print (name, age, city)
+
+         task2
+
+time = int(input("User enter second: "))
+hours = time // 3600
+minutes = (time // 60) - (hours * 60)
+seconds = time % 60
+print(f"{hours:02}:{minutes:02}:{seconds:02}")
+
+
+         task3
+
+n = input("Enter chislo: ")
+print(format("{n} + {n + n} + {n + n + n} = {int(n) + int(n + n) + int(n + n + n)}"))
+
+        task4
+
+chislo = int(input("User enter celoe polozitelnoe chislo: "))
+big_chislo = 0
+while chislo > 0:
+    chislo2 = chislo % 10
+    if chislo2 > 0
+        big_chislo = chislo2
+        if big_chislo == 9
+            break
+print ("")
